88d6fd63d2734cc47c3f6e3837cdf64cb00f9120
Ian Hamilton <ian.hamilton@dnata.com>
2019-01-27 21:40:43 +0400

    fixed unit-tests

--- lib/blinkr/config.rb
+++ lib/blinkr/config.rb
@@ -18,7 +18,8 @@ module Blinkr
 
     DEFAULTS = {
         skips: [], ignores: [], environments: [], max_retrys: 3, browser: 'chrome', report: 'blinkr.html',
-        ignore_internal: false, ignore_external: false, js_errors: false, remote: false, verbose: false
+        ignore_internal: false, ignore_external: false, js_errors: false, remote: false, verbose: false,
+        ignore_ssl: false
     }.freeze
 
     AVAILABLE_BROWSERS = %w[chrome].freeze
--- test/unit-test/config_test.rb
+++ test/unit-test/config_test.rb
@@ -7,7 +7,7 @@ class TestBlinkr < Minitest::Test
       skips: [], ignores: [], environments: [], max_retrys: 3,
       browser: 'chrome', report: 'blinkr.html',
       ignore_internal: false, ignore_external: false, js_errors: false,
-      remote: false, verbose: false
+      remote: false, verbose: false, ignore_ssl: false
   }.freeze
 
   describe Blinkr::Config do
@@ -33,7 +33,7 @@ class TestBlinkr < Minitest::Test
       args = {}
       options[:config_file] = "#{__dir__}/config/valid_blinkr.yaml"
       actual_hash = Blinkr::Config.read(options[:config_file], args)
-      expected_hash = { skips: [/^http:\/\/(www\.)?example\.com\/foo/], ignores: [], environments: [], max_retrys: 3, browser: 'chrome', report: 'foo/blinkr.html', ignore_internal: false, ignore_external: false, js_errors: false, remote: false, verbose: false, base_url: 'http://www.example.com/', threads: 20, config_file: "#{__dir__}/config/valid_blinkr.yaml" }
+      expected_hash = { skips: [/^http:\/\/(www\.)?example\.com\/foo/], ignores: [], environments: [], max_retrys: 3, browser: 'chrome', report: 'foo/blinkr.html', ignore_internal: false, ignore_external: false, js_errors: false, remote: false, verbose: false, ignore_ssl: false, base_url: 'http://www.example.com/', threads: 20, config_file: "#{__dir__}/config/valid_blinkr.yaml" }
       assert_equal expected_hash.to_s, actual_hash.to_h.to_s
     end
 
