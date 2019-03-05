286dff5633ffea6918c99986f71798f29faa78ef
Ian Hamilton <ian.hamilton@dnata.com>
2019-03-05 21:08:27 +0400

    Fix for invalid/outdated ssl certificates

--- lib/blinkr/openstruct.rb
+++ lib/blinkr/openstruct.rb
@@ -1,6 +1,5 @@
-# Monkeypatch OpenStruct
 class OpenStruct
-  EXCEPT = %I[response body resource_errors javascript_errors].freeze
+  EXCEPT = %I[response body javascript_errors].freeze
 
   def to_json(*args)
     to_h.delete_if {|k, _v| EXCEPT.include?(k)}.to_json(*args)
---
+++ lib/blinkr/ssl.rb
@@ -0,0 +1,14 @@
+module Blinkr
+
+  class << self
+    attr_accessor :ignore_ssl_errors
+  end
+
+  def ignore_ssl_errors
+    prev_setting = OpenSSL::SSL.send(:remove_const, :VERIFY_PEER)
+    OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
+    OpenSSL::SSL.send(:remove_const, :VERIFY_PEER)
+    OpenSSL::SSL.const_set(:VERIFY_PEER, prev_setting)
+  end
+
+end
